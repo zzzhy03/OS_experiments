5a26faaaea1c96f2d257f6c23a07965df35ed08e
zzzhy03 <ZhengHanYou@outlook.com>
2024-03-26 19:41:10 +0800

    first commit

---
+++ user/test_getprocs.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+  
+  int count = getprocs();
+  printf("There are %d active processes.\n", count);
+  exit(0);
+}
